392cff75377689c731e0b87b35da386d6e5842be
etooley77 <tooleye77@gmail.com>
2024-08-18 22:23:34 -0400

    rename folders

--- site_forums/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SiteForumsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'site_forums'
--- site_forums/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-    
-class LoginUser(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-
-    def __str__(self):
-        return(f'{ self.username }')
